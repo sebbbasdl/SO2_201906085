3de96296e59d0e3039e3b3334933008f02334442
sebbbasdl <sebascss22@gmail.com>
2024-02-15 10:48:56 -0600

    tiempo de espera y eleccion de proceso aleatorio

--- Practica 1/main.c
+++ Practica 1/main.c
@@ -8,43 +8,88 @@
 
 
 void main(){
-    int numero_proceso;
-    srand(time(NULL));
-
-    // Generar un número aleatorio en el rango de 1 a 3
-    numero_proceso = rand() % 3 + 1;
-
-    printf("El número aleatorio del proceso seleccionado entre 1 y 3 es: %d\n", numero_proceso);
+    
 
     int pid1, pid2, pid3;
+    int count = 10;
+    while(1){
+        int numero_proceso;
+        srand(time(NULL));
 
-    if(numero_proceso== 1)
+        // Generar un número aleatorio en el rango de 1 a 3
+        numero_proceso = rand() % 3 + 1;        
+        printf("El número aleatorio del proceso seleccionado entre 1 y 3 es: %d\n", numero_proceso);
+        
+        int espera;
+        srand(time(NULL));
+        espera = rand() % 3 + 1;
+        printf("El tiempo de espera es de : %d\n", espera) ;
+        sleep(espera);
 
-    pid_t pid = fork(); //Se realiza el fork
-    
-    if(pid == -1){
-        perror("fork");
-        exit(1);
-    }
-    
-    if(pid == 0){
+        if(numero_proceso== 1){
+            pid1=fork();
+            if(pid1 == 0){
+                printf("child[1] --> pid = %d and ppid = %d\n",getpid(), getppid());
+            }else{
+                printf("Soy el proceso padre\n");
 
-        printf("Soy proceso hijo");
-    
-    }else{
-        printf("Soy el proceso padre\n");
+                /*Se espera a que el proceso hijo termine*/
+                int status;
+                wait(&status);
 
-        /*Se espera a que el proceso hijo termine*/
-        int status;
-        wait(&status);
+                /*Se imprime el codigo de salida del proceso hijo*/
+                if(WIFEXITED(status)){
+                    printf("\nEl proceso hijo termino con estado: %d\n", WIFEXITED(status));
+                } else {
+                    printf("Ocurrio un error al terminar el proceso hijo");
+                }
 
-        /*Se imprime el codigo de salida del proceso hijo*/
-        if(WIFEXITED(status)){
-            printf("\nEl proceso hijo termino con estado: %d\n", WIFEXITED(status));
-        } else {
-            printf("Ocurrio un error al terminar el proceso hijo");
+                printf("Terminando proceso padre\n");
+            }
+
+        }else if (numero_proceso== 2) {
+            pid2 = fork();
+            if(pid2 == 0){
+                printf("child[2] --> pid = %d and ppid = %d\n",getpid(), getppid());
+            }else{
+                printf("Soy el proceso padre\n");
+
+                /*Se espera a que el proceso hijo termine*/
+                int status;
+                wait(&status);
+
+                /*Se imprime el codigo de salida del proceso hijo*/
+                if(WIFEXITED(status)){
+                    printf("\nEl proceso hijo termino con estado: %d\n", WIFEXITED(status));
+                } else {
+                    printf("Ocurrio un error al terminar el proceso hijo");
+                }
+
+                printf("Terminando proceso padre\n");
+            }
+        }else if (numero_proceso== 3) {
+            pid3 = fork();
+            if(pid3 == 0){
+                printf("child[3] --> pid = %d and ppid = %d\n",getpid(), getppid());
+            }else{
+                printf("Soy el proceso padre\n");
+
+                /*Se espera a que el proceso hijo termine*/
+                int status;
+                wait(&status);
+
+                /*Se imprime el codigo de salida del proceso hijo*/
+                if(WIFEXITED(status)){
+                    printf("\nEl proceso hijo termino con estado: %d\n", WIFEXITED(status));
+                } else {
+                    printf("Ocurrio un error al terminar el proceso hijo");
+                }
+
+                printf("Terminando proceso padre\n");
+            }
         }
 
-        printf("Terminando proceso padre\n");
     }
+
+
 }
